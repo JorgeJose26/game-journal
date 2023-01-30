32f1fd00e21a41053abd35429eb3de403cc23f54
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-01-30 14:54:49 -0500

    added header

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
 import Header from '../../Components/Header/Header';
 
 function HomePage() {
     return (
         <div>
+            <Header />
 
         </div>
     );
