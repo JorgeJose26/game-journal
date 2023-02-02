6e618ffcbaf18a57e0b2c22536eed8ea4fbd8d6b
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-02-02 17:22:59 -0500

    added card to layout

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,21 @@
 
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
+import Card from '../../Components/Card/Card';
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 function HomePage() {
     return (
         <div>
             <Header />
+            <MDBContainer>
+                <MDBRow className='mb-3'>
+                    <MDBCol size='4'>
+                        <Card />
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+
             <Footer />
 
 
